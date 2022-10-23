434aca249ad9df0c72d547995ec67fbd07f9037d
Vamsi Madhav H <60683570+vamsimadhav@users.noreply.github.com>
2022-10-23 19:37:24 +0530

    Magic 8 Ball App

---
+++ 4)Ask me Anything(Magic 8 ball)/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Angela Yu on 14/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var magicBallImageView: UIImageView!
+    
+    let ballArray = [
+        UIImage(imageLiteralResourceName: "ball1"),
+        UIImage(imageLiteralResourceName: "ball2"),
+        UIImage(imageLiteralResourceName: "ball3"),
+        UIImage(imageLiteralResourceName: "ball4"),
+        UIImage(imageLiteralResourceName: "ball5")
+    ];
+    
+    
+
+    @IBAction func button(_ sender: UIButton) {
+        magicBallImageView.image = ballArray[Int.random(in: 0...4)]
+    }
+    
+
+}
+
